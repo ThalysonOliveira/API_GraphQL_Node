61a808c29116298cf6b655f290aab2e38fe48aa2
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-05 19:12:27 -0200

    Criado função boostrap

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
+import { buildSchema } from 'graphql'
 import 'reflect-metadata'
 import './utils/connection'
+import {ApolloServer} from 'apollo-server'
 
 async function boostrap(){
-    
+    const schema = await buildSchema({
+        resolvers: []
+    })
+
+    const server = new ApolloServer({schema})
+
+    server.listen({port: 4100},()=> console.log('Running'))
+
 }
 
 boostrap()
