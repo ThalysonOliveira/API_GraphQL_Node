a0ed05347531d09e44f4cc9aeb036e941c4b9d8d
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 00:49:49 -0200

    Criado mutation

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,6 +1,15 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import Category from './Category'
 import CategorySchema from '../../models/CategorySchema'
+
+@InputType()
+class CategoryInput{
+    @Field()
+    description: String;
+    @Field()
+    name: String;
+}
+
 
 @Resolver(Category)
 class CategoryResolver{
@@ -9,6 +18,12 @@ class CategoryResolver{
     async categorys(){
         const categories = await CategorySchema.find();
         return categories
+    }
+
+    @Mutation(()=> Category)
+    async createCategory(@Arg('categoryInput')categoryInput: CategoryInput){
+        const category = await CategorySchema.create(categoryInput)
+        return category
     }
 }
 
