813ce5f6b0f013da93c31407d568ae994b21552c
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 01:03:23 -0200

    Criado resolver de video

--- src/graphql/video/VideoResolver.ts
+++ src/graphql/video/VideoResolver.ts
@@ -1,8 +1,14 @@
-import { Resolver } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
+import Video from "./Video";
+import VideoSchema from '../../models/VideoSchema'
 
-@Resolver()
+@Resolver(Video)
 class VideoResolver{
-
+    @Query(()=> [Video])
+    async videos(){
+        const videos = await VideoSchema.find() 
+        return videos
+    } 
 }
 
 export default VideoResolver
