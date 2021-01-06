980c35838003021a568561229ffe8f7af5b45ba2
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-01-06 01:15:51 -0200

    Criado mutation

--- src/graphql/video/VideoResolver.ts
+++ src/graphql/video/VideoResolver.ts
@@ -1,6 +1,18 @@
-import { Query, Resolver } from "type-graphql";
+import {InputType, Field, Query, Resolver, Mutation, Arg } from "type-graphql";
 import Video from "./Video";
 import VideoSchema from '../../models/VideoSchema'
+
+@InputType()
+class VideoInput{
+    @Field()
+    description: String;
+    @Field()
+    name: String;
+    @Field()
+    category: String;
+}
+
+
 
 @Resolver(Video)
 class VideoResolver{
@@ -9,6 +21,12 @@ class VideoResolver{
         const videos = await VideoSchema.find() 
         return videos
     } 
+
+    @Mutation(()=> [Video])
+    async createVideos(@Arg('videoInput')videoInput: VideoInput){
+        const video = await VideoSchema.create(videoInput)
+        return video
+    }
 }
 
 export default VideoResolver
